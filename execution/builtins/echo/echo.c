d998581148cbd031b1de0f3fe3b9489fee75b5b0
zibnoukh <zibnoukh@c3r9p3.1337.ma>
2024-06-26 11:53:23 +0100

    builtins

--- execution/builtins/echo/echo.c
+++ execution/builtins/echo/echo.c
@@ -6,11 +6,29 @@
 /*   By: zibnoukh <zibnoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:43:08 by msbai             #+#    #+#             */
-/*   Updated: 2024/06/26 11:33:55 by zibnoukh         ###   ########.fr       */
+/*   Updated: 2024/06/26 11:52:48 by zibnoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../minishell.h"
+
+void    print_echo(char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        write(1, &s[i++], 1);
+}
+
+t_com   *last_node(t_com *t_tmp_ls)
+{
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+}
 
 void    echo(t_com *t_tmp_ls, char *next)
 {
@@ -22,9 +40,13 @@ void    echo(t_com *t_tmp_ls, char *next)
     while (t_tmp_ls)
     {
         // printf("hanta: %s\n", t_tmp_ls->com);
-        t_tmp_ls = t_tmp_ls->next;
         count++;
-        // ft_putstr_fd(t_tmp_ls->com, 1);
+        t_tmp_ls = t_tmp_ls->next;
+        // if(t_tmp_ls->next->com == "test")
+        // if(!(ft_strncmp(t_tmp_ls->next->com, NULL, ft_strlen(t_tmp_ls->next->com)) == 0))
+        //     print_echo(" ");
+        // print_echo(t_tmp_ls->com);
+        // print_echo(t_tmp_ls->next->com);
         // ft_putstr_fd(" ", 1);
         // ft_strlcat()
         // ft_putstr_fd("\n", 1);
@@ -33,7 +55,5 @@ void    echo(t_com *t_tmp_ls, char *next)
     printf("%d\n", count);
     // write(1, "\n", 1);
 }
-
-
 
 // 1: print all strings [ex ex ex]
