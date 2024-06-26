131fabed9332a041ae73cdf2d7e6e9271e588f48
zibnoukh <zibnoukh@c3r1p1.1337.ma>
2024-06-26 18:57:02 +0100

    builtins

--- execution/builtins/echo/echo.c
+++ execution/builtins/echo/echo.c
@@ -6,54 +6,25 @@
 /*   By: zibnoukh <zibnoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:43:08 by msbai             #+#    #+#             */
-/*   Updated: 2024/06/26 11:52:48 by zibnoukh         ###   ########.fr       */
+/*   Updated: 2024/06/26 18:41:11 by zibnoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../minishell.h"
 
-void    print_echo(char *s)
+void    procces_echo(t_com *t_tmp_ls)
 {
-    int i;
-
-    i = 0;
-    while (s[i])
-        write(1, &s[i++], 1);
-}
-
-t_com   *last_node(t_com *t_tmp_ls)
-{
-    while (/* condition */)
+    while (t_tmp_ls)
     {
-        /* code */
+        t_tmp_ls = t_tmp_ls->next;
+        ft_putstr_fd(t_tmp_ls->com, 1);
+        if(t_tmp_ls->next->com != NULL)
+            ft_putstr_fd(" ", 1);
     }
-    
 }
 
 void    echo(t_com *t_tmp_ls, char *next)
 {
     (void)next;
-    // printf("next: %s\n", next);
-    int count;
-
-    count = 0;
-    while (t_tmp_ls)
-    {
-        // printf("hanta: %s\n", t_tmp_ls->com);
-        count++;
-        t_tmp_ls = t_tmp_ls->next;
-        // if(t_tmp_ls->next->com == "test")
-        // if(!(ft_strncmp(t_tmp_ls->next->com, NULL, ft_strlen(t_tmp_ls->next->com)) == 0))
-        //     print_echo(" ");
-        // print_echo(t_tmp_ls->com);
-        // print_echo(t_tmp_ls->next->com);
-        // ft_putstr_fd(" ", 1);
-        // ft_strlcat()
-        // ft_putstr_fd("\n", 1);
-        // if(!(ft_strncmp("-n", next, ft_strlen(next)) == 0))
-    }
-    printf("%d\n", count);
-    // write(1, "\n", 1);
+    procces_echo(t_tmp_ls);
 }
-
-// 1: print all strings [ex ex ex]
