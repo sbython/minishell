dc09a883d9088187f14998abd7e167ed7b395d3f
zibnoukh <zibnoukh@c4r10p8.1337.ma>
2024-07-08 00:33:00 +0100

    c

--- execution/execute/get_options.c
+++ execution/execute/get_options.c
@@ -6,23 +6,11 @@
 /*   By: zibnoukh <zibnoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:43:08 by msbai             #+#    #+#             */
-/*   Updated: 2024/07/08 00:23:57 by zibnoukh         ###   ########.fr       */
+/*   Updated: 2024/07/08 00:32:05 by zibnoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
-// void parse_command(char *cmd, char **args) {
-//     char *token;
-//     int i = 0;
-
-//     token = strtok(cmd, " ");
-//     while (token != NULL && i < 1024 - 1) {
-//         args[i++] = token;
-//         token = strtok(NULL, " ");
-//     }
-//     args[i] = NULL;
-// }
 
 void    get_options(char **options)
 {
@@ -39,7 +27,6 @@ void    get_options(char **options)
         char *f = ft_strjoin(path, options[0]);
         if(execve(f, options, NULL) == -1)
             perror("EXOT");
-        
     }
     else
     {
