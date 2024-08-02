5e8d891e79fb089fa0854cb92ed383b5df468758
zibnoukh <zibnoukh@c4r8p3.1337.ma>
2024-08-02 19:57:34 +0100

    c

--- execution/execute/execute.c
+++ execution/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: zibnoukh <zibnoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:43:08 by msbai             #+#    #+#             */
-/*   Updated: 2024/08/02 19:47:28 by zibnoukh         ###   ########.fr       */
+/*   Updated: 2024/08/02 19:57:02 by zibnoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,13 @@ int    execute(t_box *box)
     int status;
 
     status = 0;
-    if(sizeee(box) == 1 && box->node->command->options && put_builtins(box->node->command->options[0]))
+    if(sizeee(box) == 1 && box->node->command->options && put_builtins(box->node->command->options[0]) && !box->node->command->redirection)
         status = builtins(box->node->command->options, box, 1);
-    else if(sizeee(box) == 1 && box->node->command->redirection)
+    else if(sizeee(box) == 1 && box->node->command->options && put_builtins(box->node->command->options[0]) && box->node->command->redirection)
     {
         files = run_all_heardocs(box);
         ft_redirection(box, box->node->command->redirection, files[0], 0);
+        status = builtins(box->node->command->options, box, 1);
     }
     else
         status = more_then___(box);
