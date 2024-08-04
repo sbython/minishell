f67a8f485c9988257418a9068d5bde2f1aadb8c3
zibnoukh <zibnoukh@c3r2p6.1337.ma>
2024-08-04 17:28:46 +0100

    c

--- execution/execute/more_then___.c
+++ execution/execute/more_then___.c
@@ -6,7 +6,7 @@
 /*   By: zibnoukh <zibnoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:43:08 by msbai             #+#    #+#             */
-/*   Updated: 2024/08/04 17:19:29 by zibnoukh         ###   ########.fr       */
+/*   Updated: 2024/08/04 17:26:49 by zibnoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,17 @@ int more_then___(t_box *box)
     int status = 0;
     if (run_all_heardocs(box))
         return (box->exit_val);
+    int the_size = sizeee(box);
     box->pid = malloc(sizeee(box) * sizeof(int *));
+    int k = 0;
+    while (k < sizeee(box))
+        box->pid[k++] = -1;
     box->input_file = NULL;
     box->output_file = NULL;
-    while (box->node->command)
+    t_command* cmd = box->node->command;
+    while (cmd)
     {
-        if (box->node->command->next)
+        if (cmd->next)
         {
             if (pipe(fd) == -1)
             {
@@ -43,7 +48,7 @@ int more_then___(t_box *box)
         else if (box->pid[j] == 0)
         {
             handlesignal(2, box);
-            ft_redirection(box, box->node->command->redirection, box->files[i], 1);
+            ft_redirection(box, cmd->redirection, box->files[i], 1);
             if (box->input_file)
             {
                 int Getfd_input__ = open(box->input_file, O_RDONLY);
@@ -77,7 +82,7 @@ int more_then___(t_box *box)
                 }
                 close(Getfd_output__);
             }
-            else if (box->node->command->next)
+            else if (cmd->next)
             {
                 if (dup2(fd[1], 1) == -1)
                 {
@@ -90,23 +95,27 @@ int more_then___(t_box *box)
         }
         if (prev_fd != -1)
             close(prev_fd);
-        if (box->node->command->next)
+        if (cmd->next)
         {
             close(fd[1]);
             prev_fd = fd[0];
         }
         i++;
         j++;
-        box->node->command = box->node->command->next;
+        cmd = cmd->next;
     }
     j = 0;
-    while (waitpid(box->pid[j++], &status, 0) > 0);
-    if (WIFSIGNALED(status))
+    while (j < the_size && box->pid[j] != -1)
     {
-        write(0, "\n", 1);
-        box->exit_val = WTERMSIG(status) + 128;
+        while (waitpid(box->pid[j], &status, 0) > 0);
+        if (WIFSIGNALED(status))
+        {
+            write(0, "\n", 1);
+            box->exit_val = WTERMSIG(status) + 128;
+        }
+        else
+            box->exit_val = WEXITSTATUS(status);
+        j++;
     }
-    else
-        box->exit_val = WEXITSTATUS(status);
     return box->exit_val;
 }
