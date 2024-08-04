1a83f230f33fe83bc5e159502d8d471014629f41
zibnoukh <zibnoukh@c3r2p6.1337.ma>
2024-08-04 19:46:46 +0100

    c

--- execution/execute/random_file.c
+++ execution/execute/random_file.c
@@ -6,7 +6,7 @@
 /*   By: zibnoukh <zibnoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:43:08 by msbai             #+#    #+#             */
-/*   Updated: 2024/08/02 16:40:24 by zibnoukh         ###   ########.fr       */
+/*   Updated: 2024/08/04 19:46:38 by zibnoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,7 @@ char	*random_file(char *file, int c)
 		i++;
 	}
 	new_file[i] = '\0';
-	e = ft_strdup("ttyname(0)");
-	put = ft_substr(e, ft_strlen(e) - 1, ft_strlen(e) + 1);
-	join_file = ft_strjoin(ft_strjoin("/tmp/0X", ft_itoa(ft_strlen(file) * (1060 + c)
-					/ 10)), ft_strjoin(new_file, put));
+	join_file = ft_strjoin("/tmp/", new_file);
+	free(new_file);
 	return (join_file);
 }
